bca69581f99820f2052bc890077d77eb5e89de41
Nohai Bogdan <nohai.ionut02@gmail.com>
2017-04-09 10:01:13 +0300

    Adaug primul script si primul stil, pentru pagina index, interfata pentru mobil

---
+++ scripts/script1.js
@@ -0,0 +1,51 @@
+
+var btnClose = document.getElementById("close-left-menu-btn");
+var menuLeft = document.getElementById("one");
+var menuAddTask = document.getElementById("container-info-add-task");
+var btnOpen = document.getElementById("open-left-menu-btn");
+var btnOpenTask = document.getElementById("add-task-button");
+var btnCloseAddTask = document.getElementById("close-add-task");
+var taskTextInput = document.getElementById("task-text-input");
+// alert(btnClose);
+
+// alert(menuLeft);
+
+
+
+function closeMenuLeft(){
+	
+	menuLeft.classList.add("close-left-menu");
+	if(menuLeft.classList.contains("open-left-menu")) {
+		menuLeft.classList.remove("open-left-menu");
+	}
+}
+
+function openMenuLeft() {
+	if(menuLeft.classList.contains("close-left-menu")) {
+		menuLeft.classList.remove("close-left-menu");
+	}
+	menuLeft.classList.add("open-left-menu");
+}
+
+
+
+function openAddTask() {
+	if(menuAddTask.classList.contains("close-container-add-task")){
+		menuAddTask.classList.remove("close-container-add-task");
+	}
+	menuAddTask.classList.add("open-container-add-task");
+	taskTextInput.focus();
+}
+
+function closeAddTask() {
+	if(menuAddTask.classList.contains("open-container-add-task")){
+		menuAddTask.classList.remove("open-container-add-task");
+	}
+	menuAddTask.classList.add("close-container-add-task");
+}
+
+
+btnOpen.addEventListener("click", openMenuLeft);
+btnClose.addEventListener("click", closeMenuLeft);
+btnOpenTask.addEventListener("click", openAddTask);
+btnCloseAddTask.addEventListener("click", closeAddTask);
